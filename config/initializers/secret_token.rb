b9f48b5d73c085bc02e899eb3b28b781f60b2723
patscan <patscan@gmail.com>
2013-06-11 14:04:03 -0700

    First commit
    New rails app boilerplate.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RafflerAngular::Application.config.secret_token = 'b7e09a153d3f51698014d6ba3b5be64770827fcfbdf8c767c2d88f14268d7b43d4683df940cd16418c8b97f0dbdecad2c926355209744a189728504424e11db9'
